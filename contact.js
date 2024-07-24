4d6401f4ebd99958024a33a86afc1ce3341d54e4
Shrutilap <150513066+Shrutilap@users.noreply.github.com>
2024-07-24 20:24:11 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,21 +1,21 @@
 var wa_button = document.querySelector(".wa-button");
 wa_button.addEventListener("click", handleClick);
 function handleClick(){
-    window.location.href = "https://wa.me/+917888002878?text=Reach out to us on whatsapp for any queries.";
+    window.location.href = "";
 
 }
 
 var mail_button = document.querySelector(".mail-button");
 mail_button.addEventListener("click", mailClick);
 function mailClick(){
-    window.location.href = "mailto:shrutilap01@gmail.com";
+    window.location.href = "mailto:xxx01@gmail.com";
 
 }
 
 var phone_button = document.querySelector(".phone-button");
 phone_button.addEventListener("click", phoneClick);
 function phoneClick(){
-    window.location.href = "tel:+917888002878";
+    window.location.href = "7568";
 
 }
 
